2df50cce83148c8dc12d9ae765a5d344ca767735
huiyaoren <wslshanlin@sina.com>
2016-07-03 23:49:25 +0800

    scene_switch

--- app/static/js/Database.js
+++ app/static/js/Database.js
@@ -3,15 +3,18 @@
  */
 
 // 执行 SQL
-function execute_sql(db, sql, data) {
+function execute_sql(db, sql, data, fn) {
     db.transaction(function (tx) {
         tx.executeSql(
             sql,
             data,
-            function () {
+            function (tt, result) {
+                if (fn) {
+                    fn(result)
+                }
             },
             function (tx, error) {
-                //console.log(error.message)
+                console.log(error.message)
             }
         );
     });
@@ -19,7 +22,9 @@ function execute_sql(db, sql, data) {
 
 // 格式化 字段属性名
 function get_attr_name(attr) {
+
     var name = '';
+
     for (var i in attr) {
         name += i + ', ';
     }
@@ -30,32 +35,38 @@ function get_attr_name(attr) {
 
 // 格式化 字段属性项
 function get_attr_item(attr) {
+
     var item = '';
+
     for (var i in attr) {
         item += i + ' ' + attr[i] + ', \n';
     }
     item = item.slice(0, -3);
     //console.log(item);
+
     return item
 }
 
 // 格式化 问号
 function create_question(attr) {
+
     var question = '';
+
     for (var i in attr) {
         question += '?, '
     }
     question = question.slice(0, -2);
     //console.log(question);
+
     return question
 }
 
 // ---------------------------------------------------------------------------
 
-
 function TB_User(db) {
 
     var self = this;
+
     this.table_name = 'tb_User';
     this.table_attr = {
         'username': 'text primary key',
@@ -67,24 +78,33 @@ function TB_User(db) {
         'using_staff': 'text',
         'record': 'text'
     };
-    //get_attr_name(this.table_attr)
-    //get_attr_item(this.table_attr);
-    //create_question(this.table_attr);
+
+    // 查询
+    this.select = function (data, fn) {
+        var sql = "\
+            select " + data[0] + " from " + this.table_name + "\
+            where " + data[1] + "\
+        ";
+        execute_sql(db, sql, null, function (result) {
+            fn(result)
+        });
+    };
 
     // 插入 数据
     this.insert = function (data) {
-        var sql = "insert into tb_User("
+        var sql = "insert into " + this.table_name + "("
             + get_attr_name(this.table_attr)
-            + ") values(" + create_question(this.table_attr) + ")";
+            + ") values("
+            + create_question(this.table_attr)
+            + ")";
         execute_sql(db, sql, data);
     };
 
-    // todo 修改 数据
-    this.update = function () {
-        //var sql = "update t_employee set "+(empsex='女', salary=15000) +"where "+(empid=1005);
+    // 修改 数据
+    this.update = function (data) {
+        var sql = "update " + this.table_name + " set " + data[0] + " where " + data[1];
         execute_sql(db, sql);
     };
-    //update t_employee set empsex='女', salary=15000 where empid=1005;
 
 
     // 删除 表格
@@ -104,6 +124,7 @@ function TB_User(db) {
 
     this.create();
 }
+
 // ---------------------------------------------------------------------------
 
 function TB_Biker(db) {
@@ -111,30 +132,169 @@ function TB_Biker(db) {
     this.table_name = 'tb_Biker';
     this.table_attr = {
         'role_id': 'integer primary key',
-        'skill': 'text'
+        'skill': 'text',
+        'name': 'text',
+        'price':'integer'
+    };
+
+    // 查询
+    this.select = function (data, fn) {
+        var sql = "\
+            select " + data[0] + " from " + this.table_name + "\
+            where " + data[1] + "\
+        ";
+        execute_sql(db, sql, null, function (result) {
+            fn(result)
+        });
+    };
+
+    // 插入 数据
+    this.insert = function (data) {
+        var sql = "insert into " + this.table_name + "("
+            + get_attr_name(this.table_attr)
+            + ") values("
+            + create_question(this.table_attr)
+            + ")";
+
+        execute_sql(db, sql, data);
+    };
+
+
+    // 删除 表格
+    this.drop = function () {
+        var sql = "drop table if exists " + self.table_name;
+        execute_sql(db, sql);
+    };
+
+    // 创建 表格
+    this.create = function () {
+        var sql = "\
+            create table if not exists " + self.table_name + "(\
+                " + get_attr_item(this.table_attr) + "\
+            )";
+
+        execute_sql(db, sql);
     };
 }
+
+// ---------------------------------------------------------------------------
+
 function TB_Moto(db) {
     var self = this;
     this.table_name = 'tb_Moto';
     this.table_attr = {
         'moto_id': 'integer primary key',
-        'max_speed': 'integer'
+        'max_speed': 'integer',
+        'name':'text',
+        'price':'integer'
+    };
+
+    // 插入 数据
+    this.insert = function (data) {
+        var sql = "insert into " + this.table_name + "("
+            + get_attr_name(this.table_attr)
+            + ") values("
+            + create_question(this.table_attr)
+            + ")";
+
+        execute_sql(db, sql, data);
+    };
+
+
+    // 删除 表格
+    this.drop = function () {
+        var sql = "drop table if exists " + self.table_name;
+        execute_sql(db, sql);
+    };
+
+    // 创建 表格
+    this.create = function () {
+        var sql = "\
+            create table if not exists " + self.table_name + "(\
+                " + get_attr_item(this.table_attr) + "\
+            )";
+
+        execute_sql(db, sql);
     };
 }
+
+// ---------------------------------------------------------------------------
+
 function TB_Wheel(db) {
     var self = this;
     this.table_name = 'tb_Wheel';
     this.table_attr = {
         'wheel_id': 'integer primary key',
-        'turning_speed': 'integer'
+        'turning_speed': 'integer',
+        'name':'text',
+        'price':'integer'
+    };
+
+    // 插入 数据
+    this.insert = function (data) {
+        var sql = "insert into " + this.table_name + "("
+            + get_attr_name(this.table_attr)
+            + ") values("
+            + create_question(this.table_attr)
+            + ")";
+
+        execute_sql(db, sql, data);
+    };
+
+
+    // 删除 表格
+    this.drop = function () {
+        var sql = "drop table if exists " + self.table_name;
+        execute_sql(db, sql);
+    };
+
+    // 创建 表格
+    this.create = function () {
+        var sql = "\
+            create table if not exists " + self.table_name + "(\
+                " + get_attr_item(this.table_attr) + "\
+            )";
+
+        execute_sql(db, sql);
     };
 }
+
+// ---------------------------------------------------------------------------
+
 function TB_Engine(db) {
     var self = this;
     this.table_name = 'tb_Engine';
     this.table_attr = {
         'engine_id': 'integer primary key',
-        'acceleration': 'integer'
+        'acceleration': 'integer',
+        'name':'text',
+        'price':'integer'
+    };
+
+    // 插入 数据
+    this.insert = function (data) {
+        var sql = "insert into " + this.table_name + "("
+            + get_attr_name(this.table_attr)
+            + ") values("
+            + create_question(this.table_attr)
+            + ")";
+
+        execute_sql(db, sql, data);
+    };
+
+
+    // 删除 表格
+    this.drop = function () {
+        var sql = "drop table if exists " + self.table_name;
+        execute_sql(db, sql);
+    };
+
+    // 创建 表格
+    this.create = function () {
+        var sql = "\
+            create table if not exists " + self.table_name + "(\
+                " + get_attr_item(this.table_attr) + "\
+            )";
+        execute_sql(db, sql);
     };
 }
--- app/static/js/Scene.js
+++ app/static/js/Scene.js
@@ -12,26 +12,28 @@ function LoginScene() {
 
     this.mainBody = function () {
         this.createUI();
-        console.log($me);
+        //console.log($me);
         return $me
     };
 
     this.createUI = function () {
 
-        $me.append($("\
-            <div id='_login_center'>\
-                <div id='_login_center_empty'></div>\
-                <div id='_login_center_box'>\
-                    <img src='app/static/img/userNumber.png'>\
-                    <img src='app/static/img/password.png'>\
-                    <img src='app/static/img/login_button.png'>\
-                    <img src='app/static/img/register_button.png'>\
-                    <input type='text' name='username'>\
-                    <input type='password' name='password'>\
-                </div>\
+        $me.append($('\
+            <div id="_login_center">\
+            <div id="_login_center_empty"></div>\
+            <div id="_login_center_box">\
+            <img src="app/static/img/userNumber.png">\
+            <img src="app/static/img/password.png">\
+            <img src="app/static/img/login_button.png">\
+            <img src="app/static/img/register_button.png">\
+            <form id="loginForm">\
+            <input type="text" name="username" placeholder=" 请输入账号">\
+            <input type="password" name="password" placeholder=" 请输入密码">\
+            </form>\
             </div>\
-        "));
-        console.log($me)
+            </div>\
+        '));
+        //console.log($me)
     };
 
     this.bind = function (eventName, eventFn) {
@@ -41,22 +43,47 @@ function LoginScene() {
 
     this.run = function () {
 
+        var $form = $("#loginForm");
+
         // 登陆框滑出动画
         $me.children().hide();
         $me.children().slideDown();
 
+        $form.validate({
+            rules: {
+                username: {
+                    required: true,
+                    minlength: 6,
+                    maxlength: 12
+                },
+                password: {
+                    required: true,
+                    minlength: 6,
+                    maxlength: 12
+                }
+            },
+            // todo 无效
+            message: {
+                password: {
+                    required: "adsfdasfd"
+                }
+            }
+        });
+
         // 注册
         $(_login_center_box).children("img:eq(3)").click(function () {
-            $me.trigger("clickSignButton");
+            $(document.body).trigger("clickSignButton");
             $me = $("<div id='_login'></div>");
         });
 
         // 登陆
         $(_login_center_box).children("img:eq(2)").click(function () {
-            $me.trigger("loginCorrect");
-            $me = $("<div id='_login'></div>");
-        });
+            if($form.valid()){
+                $(document.body).trigger("loginCorrect", $form);
+                $me = $("<div id='_login'></div>");
+            }
 
+        });
     }
 }
 
@@ -73,21 +100,23 @@ function SignScene() {
 
     this.createUI = function () {
 
-        $me.append("\
-            <div id='_sign_center'>\
-                <div id='_sign_center_empty'></div>\
-                <div id='_sign_center_box'>\
-                    <img src='app/static/img/registerBox.png'>\
-                    <img src='app/static/img/registerBox.png'>\
-                    <img src='app/static/img/registerBox.png'>\
-                    <img src='app/static/img/newRegister.png'>\
-                    <img src='app/static/img/reBack.png'>\
-                    <input type='text' name='username' placeholder=' 请输入账号'>\
-                    <input type='password' name='password1' placeholder=' 请输入密码'>\
-                    <input type='password' name='password2' placeholder=' 请再次输入密码'>\
-                </div>\
+        $me.append('\
+            <div id="_sign_center">\
+            <div id="_sign_center_empty"></div>\
+            <div id="_sign_center_box">\
+            <img src="app/static/img/registerBox.png">\
+            <img src="app/static/img/registerBox.png">\
+            <img src="app/static/img/registerBox.png">\
+            <img src="app/static/img/newRegister.png">\
+            <img src="app/static/img/reBack.png">\
+            <form id="signForm">\
+            <input type="text" name="username" placeholder=" 请输入账号">\
+            <input type="password" name="password1" placeholder=" 请输入密码">\
+            <input type="password" name="password2" placeholder=" 请再次输入密码">\
+            </form>\
             </div>\
-        ");
+            </div>\
+        ');
     };
 
     this.bind = function (eventName, eventFn) {
@@ -95,17 +124,57 @@ function SignScene() {
     };
 
     this.run = function () {
+
         $me.children().hide();
         $me.children().slideDown();
+        var $form = $("#signForm");
+
+        // 返回 登陆界面
         $(_sign_center_box).children("img:eq(4)").click(function () {
-            $me.trigger("returnToLogin");
+
+            $(document.body).trigger("returnToLogin");
             $me = $("<div id='_sign'></div>");
+        });
+
+        // 验证 注册信息
+        $("#signForm").validate({
+            rules: {
+                username: {
+                    required: true,
+                    minlength: 6,
+                    maxlength: 12
+                },
+                password1: {
+                    required: true,
+                    minlength: 6,
+                    maxlength: 12
+                },
+                password2: {
+                    required: true,
+                    equalTo: "[name=password1]"
+                }
+            },
+            // todo 无效
+            message: {
+                password2: {
+                    required: "两次输入不相同"
+                }
+            }
+        });
+
+        // 点击 注册按钮
+        $(_sign_center_box).children("img:eq(3)").click(function () {
+            if($("#signForm").valid()){
+                $(document.body).trigger("signSuccess", $form);
+                $me = $("<div id='_sign'></div>");
+            }
         });
     }
 }
 
 // 主界面
 function MainScene() {
+
     var self = this;
     var $me = $("<div id='_main'></div>");
 
@@ -113,6 +182,7 @@ function MainScene() {
         this.createUI();
         return $me
     };
+
     this.createUI = function () {
         $me.append('\
             <div id="_main_loading">\
@@ -135,6 +205,7 @@ function MainScene() {
             </div>\
         ')
     };
+
     this.bind = function (eventName, eventFn) {
         $(document.body).on(eventName, eventFn)
     };
@@ -142,6 +213,7 @@ function MainScene() {
     this.run = function () {
         timer = setInterval(function () {
             if ($(_main_loading_box).children("meter")[0].value < 100) {
+
                 $(_main_loading_box).children("meter")[0].value += 1;
 
                 var left = Number(
@@ -149,8 +221,6 @@ function MainScene() {
                         .children("img")
                         .css("left")
                         .slice(0, -2));
-
-                console.log(left);
 
                 $(_main_loading_box)
                     .children("img")
@@ -164,12 +234,18 @@ function MainScene() {
             }
         }, 20);
 
-         //点击 返回按钮
+        //点击 返回按钮
         $(_main_up).children("button:eq(0)").click(function () {
-            $me.trigger("returnToLogin1");
+            $(document.body).trigger("returnToLogin1");
             $me = $("<div id='_main'></div>")
-        })
+        });
+
+        // todo 商店入口
+        $(_main_down).children("button:eq(2)").click(function () {
+            $(document.body).trigger("clickShopButton");
+        });
     }
+
 }
 
 // 赛段地图选择界面
@@ -273,7 +349,7 @@ function StageScene() {
     }
 }
 
-// todo 商店
+// 商店
 function ShopScene() {
     var self = this;
     var $me = $("<div id='_shop'></div>");
@@ -284,6 +360,35 @@ function ShopScene() {
     };
     this.createUI = function () {
         $me.append('\
+        <div id="_shop_empty"></div>\
+            <div id="_shop_box">\
+                <div id="_shop_box_preview">\
+                <img src="app/static/img/effectShow.png">\
+                <h1>Null</h1>\
+                <div></div>\
+                <span>我的金币：999999</span>\
+                <button></button>\
+                <button></button>\
+            </div>\
+            <div id="_shop_box_shop">\
+                <img src="app/static/img/store.png">\
+                <img src="app/static/img/item1.png">\
+                <img src="app/static/img/item5.png">\
+                <img src="app/static/img/item6.png">\
+                <img src="app/static/img/item7.png">\
+                <div>\
+                    <div>\
+                        <div class="_shopItem">\
+                            <h2>商品名称</h2>\
+                            <img src="app/static/img/biker/c1s.png">\
+                            <span>价格：9999</span>\
+                            <button></button>\
+                            <button></button>\
+                        </div>\
+                    </div>\
+                </div>\
+            </div>\
+        </div>\
         ')
     };
     this.bind = function (eventName, eventFn) {
@@ -291,11 +396,12 @@ function ShopScene() {
     };
 
     this.run = function () {
-
+        // todo
+        $(document.body).trigger("LoadShop", $("#_shop_box_shop").children("div").children("div"))
     }
 }
 
-// todo 结算
+// 结算
 function ResultScene() {
     var self = this;
     var $me = $("<div id='_result'></div>");
@@ -306,6 +412,28 @@ function ResultScene() {
     };
     this.createUI = function () {
         $me.append('\
+        <div id="_result_up">\
+            <p>\
+            <span>难度：3星&nbsp;&nbsp; 金币：120&nbsp; 名次：6</span>\
+        <span>总数：9999</span>\
+        <span>第四名</span>\
+        <span>00:08:09</span>\
+        </p>\
+        <p>\
+        <span>00:08:09</span>\
+        <span>00:08:09</span>\
+        <span>00:08:09</span>\
+        </p>\
+        <img src="app/static/img/biker/cha2.png">\
+            <img src="app/static/img/biker/cha3.png">\
+            <img src="app/static/img/biker/cha4.png">\
+            <img src="app/static/img/biker/cha5.png">\
+            </div>\
+            <div id="_result_down">\
+            <button></button>\
+            <button></button>\
+            <button></button>\
+            </div>\
         ')
     };
     this.bind = function (eventName, eventFn) {
